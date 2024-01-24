ff1bb44a661d37675004939f601f2c44bf5f8c4c
jwc406 <77850103+jwc406@users.noreply.github.com>
2024-01-25 02:14:54 +0900

    part #5

---
+++ ReactJS_MasterClass/react-js-typescript/src/Router.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Coins from "./routes/Coins";
+import Coin from "./routes/Coin";
+
+export default function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Coins />}></Route>
+        <Route path="/:coinId" element={<Coin />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
