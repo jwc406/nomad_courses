b8071cd789ea66acd316f32a70eff970a30994b5
jwc406 <77850103+jwc406@users.noreply.github.com>
2024-01-25 04:05:32 +0900

    challenge2

--- ReactJS_MasterClass/react-js-typescript/src/index.tsx
+++ ReactJS_MasterClass/react-js-typescript/src/index.tsx
@@ -1,17 +1,17 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { theme } from "./theme";
+import ReactDOM from "react-dom";
+import { RecoilRoot } from "recoil";
 import { ThemeProvider } from "styled-components";
+import App from "./App";
+import { darkTheme } from "./theme";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>
+    <RecoilRoot>
+      <ThemeProvider theme={darkTheme}>
+        <App />
+      </ThemeProvider>
+    </RecoilRoot>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
--- ReactJS_MasterClass/react-js-typescript/src/theme.ts
+++ ReactJS_MasterClass/react-js-typescript/src/theme.ts
@@ -1,8 +1,8 @@
-import { DefaultTheme } from "styled-components/dist/types";
+import { DefaultTheme } from "styled-components";
 
-export const theme: DefaultTheme = {
+export const darkTheme: DefaultTheme = {
   bgColor: "#2f3640",
-  textColor: "#f5f6fa",
+  textColor: "white",
   accentColor: "#9c88ff",
-  btnColor: "",
+  cardBgColor: "transparent",
 };
